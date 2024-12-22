60195ebd75e0b173f0f8ca969607cc9240ec423d
velopace <velopace7@gmail.com>
2024-12-22 22:17:56 +0800

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {},
+    bsc_testnet: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY
+  }
 };
 
 export default config;
