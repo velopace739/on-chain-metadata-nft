c78cfe8ac053d1c4ca1798223db365149a0d9519
velopace <velopace7@gmail.com>
2024-12-22 22:53:03 +0800

    mint

---
+++ contracts/ChainBattles.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract ChainBattles is ERC721URIStorage {
+    using Strings for uint256;
+    uint256 private _nextTokenId;
+
+    mapping(uint256 => uint256) public tokenIdToLevels;
+
+    constructor() ERC721("Chain Battles", "CBTLS") {}
+
+    function mint() public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        tokenIdToLevels[tokenId] = 0;
+        _setTokenURI(tokenId, getTokenURI(tokenId));
+    }
+
+    function generateCharacter(uint256 tokenId) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Warrior",
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            getLevels(tokenId),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function getLevels(uint256 tokenId) public view returns (string memory) {
+        uint256 levels = tokenIdToLevels[tokenId];
+        return levels.toString();
+    }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+                '"name": "Chain Battles #', tokenId.toString(), '",',
+                '"description": "Battles on chain",',
+                '"image": "', generateCharacter(tokenId), '"',
+            "}"
+        );
+    }
+}
